a44aee706d21c4a7e74c0b36c93127d3b6576fa9
rhonig <rhonig@ibuildings.nl>
2015-03-25 21:34:08 +0100

    Add test helper method

--- test/test-helper.el
+++ test/test-helper.el
@@ -12,3 +12,17 @@
      (org-mode)
      (setq org-query-test-result (,@body))
      org-query-test-result))
+
+(defmacro org-query-element-test-map (with-seq-todo body)
+  `(lambda (_)
+     (if ,with-seq-todo
+         (save-excursion
+           (goto-char (point-min))
+           ;; Without this in the buffer org-mode will not recognize NEXT as a todo state.
+           (insert "#+SEQ_TODO: TODO NEXT | CANCELLED DONE\n")))
+     (org-mode)
+     (setq --map (org-element-parse-buffer))
+     (message "%S" --map)
+     (setq org-query-test-result (org-query-tree-map ,body --map))
+     (message "%S" org-query-test-result)
+     org-query-test-result))
