6744a8689477e14d68893c522cf3d84c5bc4552e
rhonig <rhonig@ibuildings.nl>
2015-03-25 22:56:59 +0100

    Refactored big function into smaller ones

--- test/org-query-element-test.el
+++ test/org-query-element-test.el
@@ -22,57 +22,72 @@
 ;;                      :title (#("-Inbox-" 0 7 (:parent #1)))
 ;;                      :parent #0)))
 
-(defun org-query-tree-map-visit-headline (evaluator headline ancestor-flags)
-  (let* ((fn-flag-self (plist-get algo :self))
-           (fn-parent-evaluation (plist-get algo :parents))
-           (fn-flag-children (lambda (child) (plist-get child :flags)))
-           (fn-child-flags (plist-get algo :children))
-           (fn-final-evaluation (plist-get algo :when))
-           (algo-name (plist-get algo :what))
 
-           ;; flags collected by looking at the current element in the tree
-           (flags (funcall fn-flag-self tree))
-           ;; all flags collected from all ancestors
-           (current-ancestor-flags (-distinct (-flatten (-concat ancestor-flags flags))))
-           ;; the function that recursively traverses all children
-           (fn-map-children (lambda (x) (org-query-tree-map algo x current-ancestor-flags)))
+(defun org-query-tree-map-visit-headline-self (algo headline)
+  (let* ((fn-flag-self (plist-get algo :self)))
+    (funcall fn-flag-self headline)))
 
-           ;; children of the current headline
-           (headline-children (nthcdr 2 tree))
-           ;; the result of the tree emap for the children
-           (children (mapcar fn-map-children headline-children))
-           ;; the collected flags of ALL children
-           (result-children-flags (-distinct (-flatten (mapcar fn-flag-children children))))
-           
-           ;; the flags that are set on the current result depending on the children
-           (children-flags (funcall fn-child-flags result-children-flags))
 
-           ;; an early escape evaluation function (not used yet)
-           (is-ancestor-flags-satisfied (funcall fn-parent-evaluation (-flatten ancestor-flags)))
+(defun org-query-tree-map-visit-headline-children (algo children flags ancestor-flags)
+  (let* ((current-ancestor-flags (-distinct (-flatten (-concat ancestor-flags flags))))
+         (fn-map-children (lambda (x) (org-query-tree-map algo x current-ancestor-flags))))
+    (mapcar fn-map-children children)))
 
-           ;; the structure available to the final evaluation function
-           (self `(
-                   :raw-value
-                   ,(org-element-property :raw-value tree)
-                   :todo-keyword
-                   ,(org-element-property :todo-keyword tree)
-                   :children-flags
-                   ,children-flags
-                   :flags
-                   ,flags
-                   :ancestor-flags
-                   ,(-flatten ancestor-flags)
-                   :is-ancestor-flags-satisfied
-                   ,is-ancestor-flags-satisfied))
 
-           ;; does the current result satisfy the final evaluation?
-           (satisfied (consp (funcall fn-final-evaluation self)))
+(defun org-query-tree-map-visit-headline-children-flags (algo children-results)
+  (let* ((fn-flag-children (lambda (child) (plist-get child :flags)))
+         (fn-child-flags (plist-get algo :children))
+         (result-children-flags (-distinct (-flatten (mapcar fn-flag-children children-results)))))
+    (funcall fn-child-flags result-children-flags)))
 
-           ;; create the result with name of matched algorithm
-           (result (plist-put self :satisfies (when satisfied algo-name))))
+
+(defun org-query-tree-map-visit-headline-eval-parent (algo ancestor-flags)
+  (let* ((fn-parent-evaluation (plist-get algo :parents)))
+    (funcall fn-parent-evaluation (-flatten ancestor-flags))))
+
+
+(defun org-query-tree-map-visit-headline-eval-final (algo self)
+  (let* ((fn-final-evaluation (plist-get algo :eval)))
+    (funcall fn-final-evaluation self)))
+
+
+(defun org-query-tree-map-visit-headline (evaluator headline children ancestor-flags)
+  (let* (;; flags collected by looking at the current element in the tree
+         (flags (org-query-tree-map-visit-headline-self algo tree))
+
+         ;; an early escape evaluation function (not used yet)
+         (is-ancestor-flags-satisfied (org-query-tree-map-visit-headline-eval-parent algo ancestor-flags))
+
+         ;; the result of the tree emap for the children
+         (children-results (org-query-tree-map-visit-headline-children algo children flags ancestor-flags))
+
+         ;; the flags that are set on the current result depending on the children
+         (children-flags (org-query-tree-map-visit-headline-children-flags algo children-results))
+
+         ;; the structure available to the final evaluation function
+         (self `(
+                 :raw-value
+                 ,(org-element-property :raw-value tree)
+                 :todo-keyword
+                 ,(org-element-property :todo-keyword tree)
+                 :children-flags
+                 ,children-flags
+                 :flags
+                 ,flags
+                 :ancestor-flags
+                 ,(-flatten ancestor-flags)
+                 :is-ancestor-flags-satisfied
+                 ,is-ancestor-flags-satisfied))
+
+         ;; does the current result satisfy the final evaluation?
+         (tag (org-query-tree-map-visit-headline-eval-final algo self))
+
+         ;; create the result with name of matched algorithm
+         (result (plist-put self :satisfies tag)))
 
       ;; return the children within the current result to create a tree structure
-      (append result children)))
+      (append result children-results)))
+
 
 (defun org-query-tree-map (algo tree &rest ancestor-flags)
   "Apply FN to each element of TREE while preserving the tree structure."
@@ -83,18 +98,19 @@
    ((and (listp tree) (equal (symbol-name (car tree)) "org-data"))
     (mapcar (lambda (x) (org-query-tree-map algo x)) (nthcdr 3 tree)))
    ((and (listp tree) (equal (symbol-name (car tree)) "headline"))
-    (org-query-tree-map-visit-headline algo tree ancestor-flags))
+    (let* ((children (nthcdr 2 tree))
+           (result (org-query-tree-map-visit-headline algo tree children ancestor-flags)))
+      result))
    (t nil)))
 
 
 (setq active-project
-
       '(:self ;; return flags
         (lambda (headline)
           (cond
            ((member (org-element-property :todo-keyword headline) '("NEXT")) '(is-next))
            ((member (org-element-property :todo-keyword headline) '("TODO")) '(is-todo))))
-        
+
         :parents ;; return boolean
         (lambda (flags)
           (message "flags: %S" flags)
@@ -105,13 +121,13 @@
           (message "children flags: %S" flags)
           (when (member 'is-next flags) '(has-next-task)))
         
-        :when ;; return boolean
+        :eval ;; return boolean
         (lambda (result) (message "result %S" result)
-          (and
-           (member 'has-next-task (plist-get result :children-flags))
-           (member 'is-next (plist-get result :flags))))
+          (when (and
+                 (member 'has-next-task (plist-get result :children-flags))
+                 (member 'is-next (plist-get result :flags)))
+            "active project"))))
 
-        :what "active project"))
 
 (xt-deftest org-query-test-analyse
             (xtd-return=
