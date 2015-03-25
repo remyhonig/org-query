9b2bbabfab89fbe65d53566dc2f894563f3dcb5e
rhonig <rhonig@ibuildings.nl>
2015-03-25 21:04:22 +0100

    Test for backlog tasks

--- test/org-query-gtd-test.el
+++ test/org-query-gtd-test.el
@@ -14,3 +14,11 @@
                ("* TODO A-!-\n** TODO B\n" nil)))
 
 
+(xt-deftest org-query-test-gtd-backlog-task
+  (xt-note "An active project has a NEXT todo state.")
+  (xtd-return= (org-query-test-do t (org-query-gtd-backlog-task))
+               ("* NEXT A\n** CANCELLED B\n*** TODO C-!-\n" nil)
+               ("* NEXT A\n** TODO B-!-\n" t)
+               ("* TODO A\n** TODO B-!-\n" nil)))
+
+
