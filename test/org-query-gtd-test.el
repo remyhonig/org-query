fbe50fe314c1d2f43d70cd6a5303f47f82e1a282
rhonig <rhonig@ibuildings.nl>
2015-03-25 21:03:10 +0100

    Use unquoted t for true

--- test/org-query-gtd-test.el
+++ test/org-query-gtd-test.el
@@ -1,6 +1,6 @@
 (xt-deftest org-query-test-gtd-project
   (xt-note "A project needs to have subtasks and a todo state of NEXT or TODO.")
-  (xtd-return= (org-query-test-do 't (org-query-gtd-project))
+  (xtd-return= (org-query-test-do t (org-query-gtd-project))
                ("* TODO A-!-\n** TODO B\n" t)
                ("* NEXT A-!-\n** TODO B\n" t)
                ("* TODO A-!-\n** B\n" nil)
@@ -9,7 +9,7 @@
 
 (xt-deftest org-query-test-gtd-active-project
   (xt-note "An active project has a NEXT todo state.")
-  (xtd-return= (org-query-test-do 't (org-query-gtd-active-project))
+  (xtd-return= (org-query-test-do t (org-query-gtd-active-project))
                ("* -!-NEXT A\n** TODO B\n" t)
                ("* TODO A-!-\n** TODO B\n" nil)))
 
